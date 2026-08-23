18ada9865fa259d8a51f14444d7cb5989864baf2
VictorGugug <Vgomez89@outlook.com>
2026-08-23 14:02:58 -0600

    M.E:fix: Correct resize grip and Paint tool icons

--- ZarXP/src/components/shell/Window.tsx
+++ ZarXP/src/components/shell/Window.tsx
@@ -179,7 +179,7 @@ export default function Window({ config, children }: { config: WindowConfig; chi
           <div onMouseDown={(e) => startResize(e, "ne")} style={{ position: "absolute", top: -3, right: -3, width: 8, height: 8, cursor: "ne-resize", zIndex: 11 }} />
           <div onMouseDown={(e) => startResize(e, "sw")} style={{ position: "absolute", bottom: -3, left: -3, width: 8, height: 8, cursor: "sw-resize", zIndex: 11 }} />
           <div onMouseDown={(e) => startResize(e, "se")} style={{ position: "absolute", bottom: -3, right: -3, width: 8, height: 8, cursor: "se-resize", zIndex: 11 }} />
-          <img src={assetUrl("assets/taskbar/resizegrip2.png")} alt="" style={{ position: "absolute", bottom: 1, right: 1, width: 32, height: 17, pointerEvents: "none", imageRendering: "pixelated", zIndex: 12 }} />
+          <img src={assetUrl("assets/taskbar/resizegrip2.png")} alt="" style={{ position: "absolute", bottom: 1, right: 1, width: 16, height: 14, pointerEvents: "none", imageRendering: "pixelated", zIndex: 12 }} />
         </>
       )}
     </div>
